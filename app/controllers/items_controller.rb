907923131105f8c3412a107a22e0e1ccbdd38553
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2015-07-14 17:59:01 -0400

    Fix shop params in ItemsController.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,9 +13,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Shop.find(params[:id]).items.new(item_params)
+    @item = Shop.find(params[:shop_id]).items.new(item_params)
     if @item.save
-      redirect_to shop_url(params[:id])
+      redirect_to shop_url(params[:shop_id])
     else
       render :new
     end
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:item][:id])
     if @item.update(item_params)
-      redirect_to shop_url(params[:id])
+      redirect_to shop_url(params[:shop_id])
     else
       render :edit
     end
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
   def destroy
     @item = Item.find(params[:item][:id])
     @item.destroy()
-    redirect_to shop_url(params[:id])
+    redirect_to shop_url(params[:shop_id])
   end
 
   private
