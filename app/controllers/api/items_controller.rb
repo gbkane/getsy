3c3d72107ac62ee94ba96ea1e2e2e16bba1df2a5
App Academy Student <appacademy@Apps-Mac-mini-20.local>
2015-07-15 18:39:56 -0400

    update routes in html files for api namespace.

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,68 +1,67 @@
-module Api
-  class ItemsController < ApiController
+module 
+class ItemsController < ApiController
 
-    before_action :require_signed_in!,
-      only: [:new, :create, :edit, :update]
-    before_action :require_owner, only: [:edit, :update]
+  before_action :require_signed_in!,
+    only: [:new, :create, :edit, :update]
+  before_action :require_owner, only: [:edit, :update]
 
-    def index
-      @items = Item.all
-      render :index
+  def index
+    @items = Item.all
+    render :index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    render :show
+  end
+
+  def create
+    @item = Shop.find(params[:shop_id]).items.new(item_params)
+
+    if @item.save
+      redirect_to shop_item_url(params[:shop_id],@item.id)
+    else
+      render :new
     end
+  end
 
-    def new
-      @item = Item.new
-    end
+  def edit
+    @item = Item.find(params[:id])
+    render :edit
+  end
 
-    def show
-      @item = Item.find(params[:id])
-      render :show
-    end
+  def update
+    @item = Item.find(params[:id])
 
-    def create
-      @item = Shop.find(params[:shop_id]).items.new(item_params)
+    if @item.update(item_params)
 
-      if @item.save
-        redirect_to shop_item_url(params[:shop_id],@item.id)
-      else
-        render :new
-      end
-    end
-
-    def edit
-      @item = Item.find(params[:id])
+      redirect_to shop_item_url(params[:shop_id],params[:id])
+    else
       render :edit
     end
-
-    def update
-      @item = Item.find(params[:id])
-
-      if @item.update(item_params)
-
-        redirect_to shop_item_url(params[:shop_id],params[:id])
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @item = Item.find(params[:id])
-      @item.destroy()
-      redirect_to shop_url(params[:shop_id])
-    end
-
-    private
-
-    def require_owner
-      @item = Item.find(params[:id])
-      unless @item.shop.owner_id == current_user.id
-        redirect_to shop_item_url(@item.shop_id, @item.id)
-      end
-    end
-
-    def item_params
-      params.require(:item).permit(:name, :price, :qty, :description, :main_image_url)
-    end
-
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy()
+    redirect_to shop_url(params[:shop_id])
+  end
+
+  private
+
+  def require_owner
+    @item = Item.find(params[:id])
+    unless @item.shop.owner_id == current_user.id
+      redirect_to shop_item_url(@item.shop_id, @item.id)
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :qty, :description, :main_image_url)
+  end
+
 end
