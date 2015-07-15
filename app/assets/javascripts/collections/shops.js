88fb2b1ff5765c943ab89357cde01b6273fdcae7
App Academy Student <appacademy@Apps-Mac-mini-20.local>
2015-07-15 16:52:13 -0400

    getOrFetch for Shops collection.

--- app/assets/javascripts/collections/shops.js
+++ app/assets/javascripts/collections/shops.js
@@ -1,6 +1,24 @@
 Getsy.Collections.Shops = Backbone.Collection.extend({
   url: "/shops",
-  model: Getsy.Models.Shop
+  model: Getsy.Models.Shop,
+
+  getOrFetch: function (id) {
+    var shops = this;
+    var shop = shops.get('id');
+
+    if(shop){
+      shop.fetch();
+    }else{
+      shop = new Getsy.Models.Shop({id: id});
+      shop.fetch({
+        success: function () {
+          shops.add(shop);
+        }
+      });
+    }
+
+    return shop;
+  }
 
 });
 
