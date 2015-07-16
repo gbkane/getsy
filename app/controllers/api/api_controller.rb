b7ec4b4fa05d6996f95ba00d26c4ea5dbe680c9c
gbkane <gbkane@gmail.com>
2015-07-16 18:30:39 -0400

    Change require_signed_in! to redirect to new session.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -3,7 +3,8 @@ module Api
 
     def require_signed_in!
       unless signed_in?
-        render json: ["You must be signed in to proceed!"], status: :unauthorized
+        redirect_to new_session_url
+        # render json: ["You must be signed in to proceed!"], status: :unauthorized
       end
     end
   end
