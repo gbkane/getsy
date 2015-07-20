4c049e54948bcf21335571216aca4176d6304b16
gbkane <gbkane@gmail.com>
2015-07-19 20:05:32 -0400

    serialJson file problem.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.serializeJSON.js
+//= require jquery.serializeJSON
 //= require jquery_ujs
 //= require underscore
 //= require backbone
