17d3b1ac9fff786620cd63e7c003863e8b932904
App Academy Student <appacademy@Apps-Mac-mini-8.local>
2015-07-21 14:12:17 -0400

    edit carts_controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -6,16 +6,19 @@ module Api
     end
 
     def create
-      @cart = Cart.new()
-      if @cart.save
-        render json: @cart
-      else
-        render json: "Oops, Your cart tipped over!"
-      end
+      # @cart = Cart.new()
+      # if @cart.save
+      #   render json: @cart
+      # else
+      #   render json: "Oops, Your cart tipped over!"
+      # end
+      current_cart.add(params[:data][:item_id].to_i)
+      render json: ['success']
     end
 
     def show
-      @cart = Cart.find(params[:id]);
+      # @cart = Cart.find(params[:id]);
+      @cart = current_cart
       render :show
     end
 
